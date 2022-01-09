eff1c7bf4546b9c0343ec282c5efec56ad6fc81d
onrerkc <onrekrc@gmail.com>
2022-01-09 14:07:14 +0300

    Initial commit

---
+++ version.py
@@ -0,0 +1,28 @@
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(1, 0, 12, 1),
+    prodvers=(1, 0, 12, 1),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x40004,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'040904B0',
+        [StringStruct(u'CompanyName', u'Onur Erikçi'),
+        StringStruct(u'FileDescription', u'Youtube MP3-MP4 İndirme Programı'),
+        StringStruct(u'FileVersion', u'1.0.12.1'),
+        StringStruct(u'InternalName', u'Youtube MP3-MP4 İndirme Programı'),
+        StringStruct(u'LegalCopyright', u'\xa9 Onur Erikçi. Tüm hakları saklıdır.'),
+        StringStruct(u'OriginalFilename', u'yip.exe'),
+        StringStruct(u'ProductName', u'Youtube MP3-MP4 İndirme Programı'),
+        StringStruct(u'ProductVersion', u'1.0.12.1')])
+      ]),
+    VarFileInfo([VarStruct(u'Translation', [0x041F, 1254])])
+  ]
+)
